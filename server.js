3a511d5cfab8faf3e0c8a01ce34011941acf916d
Abdul Haq <abdulhaqhk@gmail.com>
2023-06-12 21:12:24 +0530

    set all the basics for CRUD app

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+
+
+
+
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`))
+
