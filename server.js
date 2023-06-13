027276b61d8596a04895683c67fceb448bf020dc
Abdul Haq <abdulhaqhk@gmail.com>
2023-06-13 16:25:54 +0530

    post method in crud application

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
+
+//body-parser before your CRUD handlers!
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
 })
 
-
+app.post('/quote', (req, res) => {
+    console.log(req.body)
+})
 
 
 
