3d40c859730cb0f0ec8ae28cc497affb311bb584
Abdul Haq <abdulhaqhk@gmail.com>
2023-06-14 09:36:49 +0530

    Mongo atlas server connected sucessfully.

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
+// connect mongo db with node env
+
+MongoClient.connect('mongodb+srv://omeon:omeon@merncluster.eqkfofh.mongodb.net/?retryWrites=true&w=majority&appName=merncluster', (err, client) => {
+    if (err) return console.error(err)
+    console.log('Connected to Database')
+})
+
 //body-parser before your CRUD handlers!
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -13,6 +20,7 @@ app.get('/', (req, res) => {
 
 app.post('/quote', (req, res) => {
     console.log(req.body)
+    res.send('data received')
 })
 
 
